c92cdf2b3e0c38d45be271cf97a4e429d3152e8f
localadmin <miri.babayev@hotmail.com>
2020-01-17 18:07:20 +0400

    Create Model

---
+++ Budget/Budget/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Budget.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [MaxLength(50), Required]
+        public string Name { get; set; }
+
+        public User User { get; set; }
+    }
+}
